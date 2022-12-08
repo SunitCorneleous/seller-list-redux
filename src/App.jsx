5c6d05b12aff114bc9bcf61c5066824832c82de5
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-12-08 16:30:18 +0600

    seller list card added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Sellers from "./components/Sellers";
 function App() {
   return (
     <div>
-      <h1>redux app</h1>
       <Sellers></Sellers>
     </div>
   );
--- src/components/Sellers.jsx
+++ src/components/Sellers.jsx
@@ -1,3 +1,4 @@
+import { list } from "postcss";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSellers } from "../store/sellersSlice";
@@ -10,7 +11,51 @@ const Sellers = () => {
     dispatch(fetchSellers());
   }, []);
 
-  return <div></div>;
+  //   console.log(Object.keys(sellers[1]?.slotDetails?.availability));
+
+  return (
+    <div className="max-w-[1420px] mx-auto p-5">
+      <h1 className="text-4xl text-center">Total Seller: {sellers.length}</h1>
+      {sellers.map(seller => (
+        <div
+          key={seller._id}
+          className="card lg:card-side bg-base-200 my-5 shadow-xl lg:w-4/6 lg:mx-auto lg:h-[300px]"
+        >
+          <figure>
+            <img
+              src={seller.profileImg}
+              alt="Album"
+              className="lg:w-[350px] lg:object-fill lg:h-full"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Name: {seller.name}</h2>
+            <p className="text-xl">Gender: {seller.gender}</p>
+            <p className="text-xl">City: {seller.city}</p>
+            <p className="text-xl ">
+              Status:{" "}
+              <span className="text-green-600 font-semibold">
+                {seller.mentorStatus}
+              </span>
+            </p>
+            <p className="lg:text-xl">
+              Availability:{" "}
+              {Object.keys(seller?.slotDetails?.availability).map(
+                (day, index) => (
+                  <span
+                    key={index}
+                    className="badge badge-sm lg:badge-lg mx-[2px] lg:mx-1"
+                  >
+                    {day}
+                  </span>
+                )
+              )}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Sellers;
